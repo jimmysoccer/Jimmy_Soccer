f7a7c4f37cbb9b9a0b3edac3812edf35a272d3cd
Sunheng <jimmysoccer0927@gmail.com>
2023-02-20 21:27:08 -0500

    add nav-bar broswerRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import logo from "./logo.svg";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
 import "./App.css";
+import Home from "./pages/homePage";
+import Personal from "./pages/personal";
 
 function App() {
-  return <div className="App">JIMMY SOCCER</div>;
+  return (
+    <>
+      <div className="App">JIMMY SOCCER</div>
+      <BrowserRouter>
+        <Link to={"/"}>
+          <div>HOME</div>
+        </Link>
+        <Link to={"/personal"}>
+          <div>Personal Life</div>
+        </Link>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="personal" element={<Personal></Personal>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
