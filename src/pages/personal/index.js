26421b24c79c1cebb304dfa19d2cd8a65fa74c26
Sunheng <jimmysoccer0927@gmail.com>
2023-02-25 22:18:46 -0500

    add database connection

--- src/pages/personal/index.js
+++ src/pages/personal/index.js
@@ -1,3 +1,52 @@
+import "./index.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Personal() {
-  return <>personal life</>;
+  const [data, setData] = useState("");
+
+  function getData() {
+    axios
+      .get("https://cynosdb.tencentcloudapi.com", {
+        //注意使用V3方法还是V1方法
+        headers: {
+          Timestamp: Date.now(),
+          Action: "DescribeAccounts",
+          Host: "cynosdb.tencentcloudapi.com",
+          Version: "2019-01-07",
+        },
+        // params: {
+        //   Timestamp: Date.now(),
+        //   Action: "DescribeAccounts",
+        //   Version: "2019-01-07",
+        //   Region: "ap-shanghai",
+        //   ClusterId: "cynosdbmysql-b19kz1gu",
+        //   Nonce: 40,
+        //   SecretId: "",
+        //   Signature: "",
+        // },
+      })
+      .then((res) => {
+        console.log(res);
+        setData(res);
+      })
+      .catch((e) => {
+        console.log("error", e);
+        setData(e.code);
+      });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>personal life</h2>
+      <div className="data-container">
+        Mongodb --- TEST DATABASE
+        <div>data</div>
+      </div>
+    </div>
+  );
 }
