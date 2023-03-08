9c00b730540581d17045f1b13872091342d90bbf
Sunheng <jimmysoccer0927@gmail.com>
2023-03-08 11:22:59 -0500

    fix database connection

--- src/pages/personal/index.js
+++ src/pages/personal/index.js
@@ -7,7 +7,7 @@ export default function Personal() {
 
   function getData() {
     axios
-      .get("https://cynosdb.tencentcloudapi.com", {
+      .get("http://localhost:8080/getAccounts", {
         //注意使用V3方法还是V1方法
         params: {
           Timestamp: Date.now(),
@@ -16,12 +16,13 @@ export default function Personal() {
           Region: "ap-shanghai",
           ClusterId: "cynosdbmysql-b19kz1gu",
           Nonce: 40,
-          SecretId: "",
+          SecretId: "AKIDFOPmz1pPmwsjWH2lP0ZAjGi9ScuBLK5b",
           Signature: "",
         },
       })
       .then((res) => {
         console.log(res);
+        console.log("error code", res.data.Response.Error.Code);
         setData(res);
       })
       .catch((e) => {
@@ -30,8 +31,37 @@ export default function Personal() {
       });
   }
 
+  function fetchFromLocalWebServer() {
+    axios
+      .get("http://localhost:8080/", {
+        params: {
+          region: "ap-shanghai",
+          endpoint: "cynosdb.tencentcloudapi.com",
+          ClusterId: "cynosdbmysql-b19kz1gu",
+        },
+      })
+      .then((res) => {
+        console.log("fetch from local server data\n", res);
+      })
+      .catch();
+  }
+  function fetchFromSql() {
+    axios
+      .get("http://localhost:8080/", {
+        params: {
+          path: "getAllList",
+        },
+      })
+      .then((res) => {
+        console.log("fetch from sql\n", res);
+      })
+      .catch();
+  }
+
   useEffect(() => {
-    getData();
+    // getData();
+    // fetchFromLocalWebServer();
+    fetchFromSql();
   }, []);
 
   return (
