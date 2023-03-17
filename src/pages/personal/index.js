6928e59bf13d30717f71c0129c80705ff8401867
Sunheng <jimmysoccer0927@gmail.com>
2023-03-17 00:29:29 -0400

    update web server to tencent server

--- src/pages/personal/index.js
+++ src/pages/personal/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const urlLink = "https://serverservice-u2y2a.kinsta.app";
+const urlLink = "http://124.221.98.15:6000";
 const localLink = "http://localhost:8080";
 export default function Personal() {
   const [data, setData] = useState([]);
