e7c1dfa40e7e9e059181894ae0dbde18b5dfb258
jimmysoccer <jimmysoccer0927@gmail.com>
2023-12-15 17:32:14 -0500

    modify path under root dir

--- src/App.js
+++ src/App.js
@@ -1,74 +1,77 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 import {
   BrowserRouter,
   Routes,
   Route,
   Link,
   useLocation,
-} from "react-router-dom";
-import "./App.css";
-import Home from "./pages/homePage";
-import Personal from "./pages/personal";
-import Chat from "./pages/chat";
+} from 'react-router-dom';
+import './App.css';
+import Home from './pages/homePage';
+import Personal from './pages/personal';
+import Chat from './pages/chat';
 
 function App() {
   function setHeadTitle(headTitle) {
-    document.getElementById("head-title").innerText = headTitle;
+    document.getElementById('head-title').innerText = headTitle;
   }
 
   return (
     <>
       <BrowserRouter>
-        <ul className="nav-bar">
+        <ul className='nav-bar'>
           <li>
             <Link
-              to={"/Jimmy_Soccer"}
+              to={'/Jimmy_Soccer'}
               onClick={() => {
-                setHeadTitle("Jimmy | Home");
+                setHeadTitle('Jimmy | Home');
               }}
-              className="title"
+              className='title'
             >
               Jimmy Soccer
             </Link>
           </li>
           <li>
             <Link
-              to={"/Jimmy_Soccer"}
+              to={'/Jimmy_Soccer'}
               onClick={() => {
-                setHeadTitle("Jimmy | Home");
+                setHeadTitle('Jimmy | Home');
               }}
-              className={"nav-block"}
+              className={'nav-block'}
             >
               HOME
             </Link>
           </li>
           <li>
             <Link
-              to={"/developer"}
+              to={'/Jimmy_Soccer/developer'}
               onClick={() => {
-                setHeadTitle("Jimmy | Test");
+                setHeadTitle('Jimmy | Test');
               }}
-              className={"nav-block"}
+              className={'nav-block'}
             >
               Developing
             </Link>
           </li>
           <li>
             <Link
-              to={"/chat"}
+              to={'/Jimmy_Soccer/chat'}
               onClick={() => {
-                setHeadTitle("Jimmy | Chat");
+                setHeadTitle('Jimmy | Chat');
               }}
-              className={"nav-block"}
+              className={'nav-block'}
             >
               Chat
             </Link>
           </li>
         </ul>
         <Routes>
-          <Route path="/Jimmy_Soccer" element={<Home></Home>}></Route>
-          <Route path="developer" element={<Personal></Personal>}></Route>
-          <Route path="chat" element={<Chat></Chat>}></Route>
+          <Route path='/Jimmy_Soccer' element={<Home></Home>}></Route>
+          <Route
+            path='/Jimmy_Soccer/developer'
+            element={<Personal></Personal>}
+          ></Route>
+          <Route path='/Jimmy_Soccer/chat' element={<Chat></Chat>}></Route>
         </Routes>
       </BrowserRouter>
     </>
