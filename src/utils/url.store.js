ccddf5ddd76c2d8f05e1a813b0833a94acd0c061
jimmysoccer <jimmysoccer0927@gmail.com>
2024-01-19 20:19:43 -0500

    convert url store to const

--- src/utils/url.store.js
+++ src/utils/url.store.js
@@ -3,11 +3,11 @@ let fastApiPort;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   // dev code
   fastApiHost = 'http://127.0.0.1';
-  fastApiPort = import.meta.env.FASTAPI_PORT_DEV;
+  fastApiPort = 8000;
 } else {
   // production code
   fastApiHost = 'http://124.221.98.15';
-  fastApiPort = import.meta.env.FASTAPI_PORT_PROD;
+  fastApiPort = 443;
 }
 export const fastAPiUrl = `${fastApiHost}:${fastApiPort}`;
 export const educationHistoryUrl = `${fastAPiUrl}/jimmy_website/get_education_history`;
