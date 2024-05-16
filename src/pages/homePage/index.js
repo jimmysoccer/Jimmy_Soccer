eea11f4b9da12bd890c210710fa6ca04232bb38d
Sun Heng <jimmysoccer0927@gmail.com>
2024-05-15 22:41:08 -0400

    remove unecessary imports

--- src/pages/homePage/index.js
+++ src/pages/homePage/index.js
@@ -2,7 +2,6 @@ import './index.css';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import { fullStackImage } from '../../imgs/images';
 import Projects from '../projects';
 import WorkingExperience from '../workingExperience';
 import web_development_animation from '../../assets/web_development.json';
