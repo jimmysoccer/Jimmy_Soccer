548176d7ad5230238f8b14e86ad0fcf0949bfca4
Sun Heng <jimmysoccer0927@gmail.com>
2024-05-21 00:25:43 -0400

    add text to innovation hub

--- src/pages/innovationHub/InnovationHub.js
+++ src/pages/innovationHub/InnovationHub.js
@@ -1,7 +1,12 @@
 export default function InnovationHub() {
   return (
-    <div className='container my-5'>
-      <h1 className='text-center text-success'>Innovation Hub</h1>
+    <div className='container my-5 text-center'>
+      <h1 className='text-success'>Innovation Hub</h1>
+      <h3 className='text-success opacity-50'>
+        Discover groundbreaking designs and state-of-the-art tech solutions that
+        redefine the digital experience, showcasing the limitless possibilities
+        of innovation.
+      </h3>
     </div>
   );
 }
