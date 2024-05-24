5304b04e09c6393b2f0dd17d2674f51c741aad63
jimmysoccer <jimmysoccer0927@gmail.com>
2024-05-23 22:47:11 -0400

    add dev backend

--- src/pages/innovationHub/InnovationHub.js
+++ src/pages/innovationHub/InnovationHub.js
@@ -1,4 +1,20 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 export default function InnovationHub() {
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://111.229.172.53/dev/get_all_records')
+      .then((res) => {
+        const data = res?.data;
+        setRecords(data);
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
+  }, []);
+
   return (
     <div className='container my-5 text-center'>
       <h1 className='text-success'>Innovation Hub</h1>
@@ -7,6 +23,9 @@ export default function InnovationHub() {
         redefine the digital experience, showcasing the limitless possibilities
         of innovation.
       </h3>
+      {records.map((record) => (
+        <div>{record?.first_name}</div>
+      ))}
     </div>
   );
 }
